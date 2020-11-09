0083d1ff2874bf6c8b7d76409e151f4e7167ec51
ayline <ayline1695@hotmail.com>
2020-11-09 23:35:27 +0100

    pista

--- js/game.js
+++ js/game.js
@@ -1,33 +1,68 @@
 "use strict";
 class Game{
     constructor(canvas){
-       this.canvas = canvas;
+        this.canvas = canvas;
         this.context = this.canvas.getContext("2d");
         this.player;
         this.ball = [];
         this.isGameOver = false;
+        //empate
+        this.isDeadHeath = false;
     }
  
     start(){
-        this.drawBackground();
-        const player = new Players(this.canvas,2);
-        player.move();
-        const ball = new Ball(canvas);
-        ball.draw();
         
+        this.player = new Players(this.canvas,2);
+        
+        // cantidad random de pelotas
+        const loop = () => {
+            this.ball = new Ball(this.canvas,1);
 
-     //  if (!this.isGameOver) {
-     //      this.isGameOver = true;
-     //      window.requestAnimationFrame(loop);
-     //    }
-     //    window.requestAnimationFrame(loop);
+        //const ball = new Ball(canvas);
+        //this.ball.draw();
+        this.drawCanvas();
+        this.checkAllCollisions();
+        this.updateCanvas();
+        this.clearCanvas();
+        this.drawCanvas();
+        //this.player.move();
+ 
+        if (!this.isGameOver) {
+            window.requestAnimationFrame(loop);
+          }
+        };
+
+          window.requestAnimationFrame(loop);
     }
 
-    drawBackground(){
+    updateCanvas(){
+        this.player.update();
+        this.ball.update();
+    }
+
+    clearCanvas(){
+        this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
+    }
+
+    drawCanvas(){
         this.canvas = document.querySelector("canvas");
-        this.context.strokeRect(300,0,400,500);
-        this.context.strokeRect(700,0,400,500);
+        this.context.beginPath();
+        this.context.strokeRect(0,0,this.canvas.width/2,this.canvas.height);
+        this.context.strokeRect(0,0,this.canvas.width+2,this.canvas.height);
+        this.context.fillStyle = "#ffffff";
+        this.context.stroke();
+        this.context.closePath();
+
+        this.player.draw();
+        this.ball.draw();
     }
-    
+
+    checkAllCollisions(){
+        this.player.checkScreen();
+        this.ball.colllisionBall();
+    }
+    gameOverCallback(callback) {
+        this.onGameOver = callback;
+    }
    
 }
--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
 "use-strict"
 
-//window.onload = () => {
-//  document.getElementById('start-button').onclick = () => {
-//    startGame();
-//  };
-//
-// function startGame() {
-//    const myGame = new Game();
-//    myGame.init();
-//    
-// } 
-// }
-
  const main = () => {
    const buildDom = (html) => {
      const main = document.querySelector('main');
      main.innerHTML = html;
    }
+
   const buildScreen = () => {
     buildDom(`
     <div class="game-intro">
@@ -27,52 +16,48 @@
     <button id="start-button">Start Game</button>
     <p>Use<br/>UP and DOWN arrow o W and S<br/>to control the paddles!</p>
   </div>`);
-  const startButton = document.querySelector("#start-button");
-  startButton.addEventListener("click", buildGame)
+    const startButton = document.querySelector("button");
+    startButton.addEventListener("click", buildGame)
   }
+
   const buildGame = () =>{
     buildDom(`
-    <div>funciona?</div>
-    <div id="game-board">
-    <canvas id="canvas" width="600" height="500"></canvas>
-    </div>
-  `);
-  const width = document.querySelector("#game-board").offsetWidth;
-  const height = document.querySelector("#game-board").offsetHeight;
+      <div class="game-board">
+      <canvas id="canvas"></canvas>
+      </div>
+            `);
+
+  const width = document.querySelector(".game-board").offsetWidth;
+  const height = document.querySelector(".game-board").offsetHeight;
 
   const canvasElement = document.querySelector("#canvas");
 
-  canvasElement.setAttribute("width",width);
+  canvasElement.setAttribute("width", width);
   canvasElement.setAttribute("height", height);
+
 // enlace a pagina GAME
-  const game = new Game(canvasElement)
+  const game = new Game(canvasElement);
 
   // Win / Lose / Equal
-  //game.gameOverCallback(buildGameOver);
+  game.gameOverCallback(buildGameOver);
 
   game.start();
 
-// const setPlayersDirection = event => {
-//    const key = event.keyCode;
-//    const keysUsefull = [38,40,87,83];
-//    if (keysUsefull.includes(key)){
-//        event.preventDefault();
-//        switch (key){
-//            case 38:
-//            case 87:
-//                if (this.y >= 10) this.x -= 20;
-//            break;
-//            case 40:
-//            case 83:
-//                if (this.x <= 490 - this.width) this.x += 20;
-//            break;
-//        }
-//    }
-//
-// };
-// document.addEventListener("keydown", setPlayersDirection)
+  // probar con esto, e ir probando despues con lo hecho
+ //const setPlayerDirection = (event) => {
+ //  if (event.code === "ArrowUp") {
+ //    game.player.setDirection(-1);
+ //  } else if (event.code === "ArrowDown") {
+ //    game.player.setDirection(1);
+ //  }
+ //};
 
-  };
+ //document.addEventListener("keydown", setPlayerDirection);
+
+ 
+
+  }
+
   const buildGameOver = () =>{
     buildDom(`<div class="game-intro">
     <p>Loser!</p>
@@ -82,11 +67,14 @@
     <button class="restart">Restart</button>
     
   </div>`);
-  const restartButton = document.querySelector(".restart");
-  restartButton.addEventListener("click", buildGame)
+
+    const restartButton = document.querySelector(".restart");
+    restartButton.addEventListener("click", buildGame)
   };
 
   buildScreen();
- }
+  
+ };
 
+ // aqui podría intentar poner el refrescar pestaña segun la plataforma
  window.addEventListener("load", main)
