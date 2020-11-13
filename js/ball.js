19836097257420907639151861b6c9f9fb84cb82
ayline <ayline1695@hotmail.com>
2020-11-13 16:22:46 +0100

    ball

--- js/ball.js
+++ js/ball.js
@@ -21,9 +21,7 @@ class Ball {
         if(this.x + this.dx > this.canvas.width-this.ballRadius || this.x + this.dx < this.ballRadius) {
             this.dx = -this.dx;
         }
-        if(this.y + this.dy > this.canvas.height-this.ballRadius || this.y + this.dy < this.ballRadius) {
-            this.dy = -this.dy;
-        }
+  
         this.x += this.dx;
         this.y += this.dy;
     }
@@ -37,19 +35,68 @@ class Ball {
         
     }
     collisionBall(playerOne,playerTwo){
-        const colliArriba = this.y + this.dy / 2 > playerOne.y - playerOne.height / 2;
-        const colliAbajo = this.y - this.dy / 2 < playerOne.y + playerOne.size / 2;
-        const colliIzquierda = this.x - this.dx < playerTwo.x + playerTwo.size / 2;
-        const colliDerecha = this.x + this.dx / 2 > playerOne.x - playerOne.size / 2;
-        if (colliIzquierda) {
-            console.log("izquierda");
-            this.dy = -this.dy;
-        }
-        return false;
-        
-        
-    }
+       // colision arriba y abajo
+if(this.y + this.dy > this.canvas.height-this.ballRadius || this.y + this.dy < this.ballRadius){
+    this.dy = -this.dy;
+}
+// izquierda /derecha
+else if(this.x + this.dx > this.canvas.width-this.ballRadius|| this.x + this.dx < this.ballRadius) {
 
+ //  if (this.y  > playerOne.y && this.y  < playerOne.y + playerOne.height || this.y  > playerTwo.y && this.y  < playerTwo.y + playerTwo.height){
+ //      // va mas rapido cada vez que le da a la barra
+ //     this.dy += +this.dy;
+ //      // rebota en la barra
+ //      this.dx = -this.dx;
+ //  }
+    if (this.y  > playerOne.y && this.y  < playerOne.y + playerOne.height){
+        // va mas rapido cada vez que le da a la barra
+       this.dy += +this.dy;
+        // rebota en la barra
+        this.dx = -this.dx;
+    }
+    // si no, se reinicia el juego
+    else {
+         // se resta la 1 de las 3 vidas
+        playerOne.lives--;
+        playerTwo.score++;
+
+        if(!playerOne.lives){
+            alert("GAME OVER");
+            document.location.reload();
+        }
+        else { // volver a poner en la posición inicial
+            this.x = this.canvas.width/2;
+            this.y = this.canvas.height/2;
+            this.dx = 2;
+            this.dy = +2;
+        }
+    }
+    if (this.y  > playerTwo.y && this.y  < playerTwo.y + playerTwo.height){
+        // va mas rapido cada vez que le da a la barra
+       this.dy += +this.dy;
+        // rebota en la barra
+        this.dx = -this.dx;
+    }
+    // si no, se reinicia el juego
+    else {
+         // se resta la 1 de las 3 vidas
+        playerTwo.lives--;
+        playerOne.score++;
+
+        if(!playerTwo.lives){
+            alert("GAME OVER");
+            document.location.reload();
+        }
+        else { // volver a poner en la posición inicial
+            this.x = this.canvas.width/2;
+            this.y = this.canvas.height/2;
+            this.dx = 2;
+            this.dy = +2;
+        }
+    }
+    
+}
+    }
     checkCollisionEnemy(enemy) {
         const collideRight = this.x + this.dx / 2 > playerOne.x - playerTwo.size / 2;
         const collideLeft = this.x - this.dx / 2 < playerTwo.x + playerTwo.size / 2;
